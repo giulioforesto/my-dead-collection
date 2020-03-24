fae90adb9a23cfffa1d224c7d4fea0b2b29920b5
Giulio Foresto <giulioforesto@gmail.com>
2020-03-24 17:14:01 +0100

    F added unlimited search

--- MyDeadCollection.js
+++ MyDeadCollection.js
@@ -8,9 +8,9 @@ var searchOptions = require(process.cwd()+'/searchOptions.js');
 
 var sourceURL = "https://www.libramemoria.com/avis?";
 
-for (let key in searchOptions) {
-	sourceURL += key + "=" + searchOptions[key] + "&";
-}
+// for (let key in searchOptions.options) {
+	// sourceURL += key + "=" + searchOptions[key] + "&";
+// }
 
 var options = {
 	headers: {
@@ -29,11 +29,13 @@ var options = {
 	}
 }
 
-var recordResults = function (doc, page) {
+var recordResults = function (doc, dpt, year, month, page) {
 	let output = "";
 	doc.querySelectorAll('div.tableau_liste').forEach( (table) => {
 		let date = table.querySelector('div.titre').textContent.replace(/[^1-9\/]*/, "");
-		table.querySelectorAll('div[class=ligne]').forEach( (line) => {
+		let lignes = table.querySelectorAll('div[class=ligne]');
+		lastPageWarning = 
+		lignes.forEach( (line) => {
 			try {
 				let result = [];
 				let nomCellArray = line.querySelector('div.nom').textContent
@@ -45,17 +47,17 @@ var recordResults = function (doc, page) {
 					.replace(/ *\(.*/g, "")
 					.split("\n");
 				
-				result[0] = nomCellArray[0];
-				result[1] = nomCellArray[1];
-				result[2] = nomCellArray[2] ? nomCellArray[2].replace(/née /,"") : "";
-				result[3] = "";
+				result[0] = nomCellArray[0]; // Prénom
+				result[1] = nomCellArray[1]; // Nom
+				result[2] = nomCellArray[2] ? nomCellArray[2].replace(/née /,"") : ""; // Date de naissance
+				result[3] = ""; // Ville(s)
 				line.querySelectorAll('div.ville > a').forEach( (commune) => {
 					result[3] += (result[3] ? "," : "") + commune.textContent;
 				});
-				result[4] = line.querySelector('div.age').textContent.replace(/[\t ]+$/, "").replace(/^[\t ]+/, "");
-				result[5] = line.querySelector('div.titre_journal').textContent.replace(/[\t ]+$/, "").replace(/^[\t ]+/, "");
-				result[6] = date;
-				result[7] = line.querySelector('a').href;
+				result[4] = line.querySelector('div.age').textContent.replace(/[\t ]+$/, "").replace(/^[\t ]+/, ""); // Age
+				result[5] = line.querySelector('div.titre_journal > a').textContent.replace(/[\t ]+$/, "").replace(/^[\t ]+/, ""); // Journal
+				result[6] = date; // Date de publication
+				result[7] = line.querySelector('a').href; // Lien
 				
 				output += result.join(";") + "\r\n";
 			}
@@ -68,33 +70,66 @@ var recordResults = function (doc, page) {
 
 	outputStream.write(output, (err) => {
 		if (err) throw err;
-		else console.log("Completed page " + page);
+		else {
+			console.log("Completed dpt " + dpt + ", year " + year + ", month " + month + ", page " + page);
+			if (page == 20) {
+				
+			}
 	});
 }
 
 var outputStream = fs.createWriteStream('output.csv');
 outputStream.write("Prenom;Nom;Nom JF;Commune;Age;Journal;Date;Lien\r\n");
+
+let dptIterator = 0
+
+let searchDate = new Date(searchOptions.data.startDate);
+
+for (let dptIterator = 0; dptIterator < searchOptions.data.departements.length(); dptIterator++) {
+	let localDptIterator = dptIterator;
+	let dateLoop = setInterval ( () => { // Interval is set in order not to send all the HTTP requests at the same time
+		let month = searchDate.getMonth(); // 0-11
+		let year = searchDate.getFullYear();
 		
-let k = 0; // Page iterator
-let loop = setInterval ( () => { // For each page. Interval is set in order not to send all the HTTP requests at the same time.
-	k++;
-	let i = k;
-	let pageURL = sourceURL + "&page=" + i;
-	https.get(pageURL, options, (pageRes) => {
-		let pageData = '';
-		pageRes.on('data', (chunk) => {
-			pageData += chunk;
-		});
-		pageRes.on('end', () => {
-			const pageDoc = (new JSDOM (pageData)).window.document;
-			if (pageDoc.querySelector('p.noresults')) { // If page is empty raises the signal (k=0) to stop setInterval
-				if (k != 0) {console.log("Last page reached");}
-				k=0;
-				clearInterval(loop);
-			}
-			else { recordResults(pageDoc, i); }
-		});
-	}).on("error", (err) => {
-		console.log("Error: " + err.message);
-	});
-}, 100);
+		// Get the last day of the month
+		searchDate.setMonth(month+1);
+		searchDate.setDate(0);
+		let lastDayOfMonth = searchDate.getDate();
+		searchDate.setDate(1); // Increment date iterator
+		searchDate.setMonth(month+1);
+		
+		if (searchDate > new Date()) {clearInterval(dateLoop);}
+			
+		let dateURL = sourceURL
+			+ "&departement=" + searchOptions.data.departements[localDptIterator]
+			+ "&debut=1/" + month + "/" + year
+			+ "&fin=" + lastDayOfMonth + "/" + month + "/" + year;
+			
+		let page = 0; // Page iterator
+		let pageLoop = setInterval ( () => { // For each page
+			page++;
+			let localPage = page;
+			let pageURL = dateURL + "&page=" + localPage;
+			https.get(pageURL, options, (pageRes) => {
+				let pageData = '';
+				pageRes.on('data', (chunk) => {
+					pageData += chunk;
+				});
+				pageRes.on('end', () => {
+					const pageDoc = (new JSDOM (pageData)).window.document;
+					if (pageDoc.querySelector('p.noresults')) { // If page is empty stop setInterval
+						clearInterval(pageLoop);
+					}
+					else { recordResults(
+						pageDoc,
+						searchOptions.data.departements[localDptIterator],
+						year,
+						month,
+						localPage); }
+				});
+			}).on("error", (err) => {
+				console.log("Error: " + err.message);
+			});
+		}, 100);
+	}, 97);
+}
